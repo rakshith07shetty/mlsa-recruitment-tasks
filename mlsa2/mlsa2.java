fcdd2b516afc1c7a840659a6c4e0618f306624c7
rakshith07shetty <75131105+rakshith07shetty@users.noreply.github.com>
2020-11-28 17:49:25 +0530

    Add files via upload

---
+++ mlsa2/mlsa2.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class mlsa2
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        int n,k;
+        String inp,output;
+        System.out.println("Enter n and k:");
+        n=in.nextInt();
+        k=in.nextInt();
+        char[] c = new char [n];
+        
+        System.out.println("enter the string (keep it to n characters)(only first n characters will be taken:");
+        inp = in.nextLine();
+        
+        inp.getChars(0,n,c,0);
+        
+        System.out.println("Enter the letters:");
+        char[] test = new char[k];
+        
+        for (int i=0;i<k;i++)
+        {
+            System.out.println("Enter :"+(i+1)+"letter");
+            test[i]=in.next().charAt(0);
+        }
+        int no =0;
+        for(int i=0;i<n;i++)
+        { for (int j=0;j<k;j++)
+            {
+           if (inp.charAt(i)==test[j]){
+           no ++;
+           System.out.println(inp.charAt(i)+"  ");}
+            }
+            
+         
+        
+        
+        
+       
+     
+        }
+    }   
+}
